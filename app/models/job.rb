4faa9d9cf3123eb74fc74fb6c9e959f7777a532d
maro150cm <haveagoodday8496@gmail.com>
2015-04-02 03:30:00 +0000

    model job add

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,10 @@
 class Job < ActiveRecord::Base
+    validates :title, presence: { message: '請填寫招文標題' }
+    validates :job_type, presence: { message: '請填寫工作類型' }
+    validates :job_name, presence: { message: '請填寫工作職稱' }
+    validates :company_name, presence: { message: '請填寫公司名稱' }
+    validates :location, presence: { message: '請填寫公司地址' }
+    validates :description, presence: { message: '請填寫招文內容' }
+    validates :apply_information, presence: { message: '請填寫允許投遞資料' }
+    validates :deadline, presence: { message: '請填寫截止日期' }
 end
